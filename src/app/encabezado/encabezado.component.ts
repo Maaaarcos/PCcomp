b8002f8e0cc684ce17b3897404a7552b6593440b
Maaaarcos <1733280@alu.murciaeduca.es>
2024-02-18 20:47:49 +0100

    First Commit

---
+++ src/app/encabezado/encabezado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encabezado',
+  standalone: true,
+  imports: [EncabezadoComponent],
+  templateUrl: './encabezado.component.html',
+  styleUrl: './encabezado.component.css'
+})
+export class EncabezadoComponent {
+
+}
